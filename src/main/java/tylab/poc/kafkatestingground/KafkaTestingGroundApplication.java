69b7ede6362a2b4a45a093ffe0e7c38de800ca25
tivon <tivonyuen@outlook.com>
2023-01-01 21:53:35 +0800

    initial commit

---
+++ src/main/java/tylab/poc/kafkatestingground/KafkaTestingGroundApplication.java
@@ -0,0 +1,13 @@
+package tylab.poc.kafkatestingground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTestingGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaTestingGroundApplication.class, args);
+	}
+
+}
